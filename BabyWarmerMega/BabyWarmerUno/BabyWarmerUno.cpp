589e187c2b82dfa1974248d6cebf1d9f1777d141
Abhish Khanal <072bex402@ioe.edu.np>
2019-12-07 11:14:25 +0545

    all working code

---
+++ BabyWarmerMega/BabyWarmerUno/BabyWarmerUno.cpp
@@ -0,0 +1,532 @@
+/*
+ * MasterBabyWarmer.cpp
+ *
+ * Created: 11/8/2019 12:45:14 PM
+ *  Author: NIC
+ */ 
+
+
+/*
+ * MasterIncubator.cpp
+ *
+ * Created: 6/6/2019 8:01:04 AM
+ *  Author: swain
+ */ 
+
+/*
+ * MasterBabyWarmer.cpp
+ *
+ * Created: 11/10/2019 10:05:04 AM
+ *  Co-Author: Abhish Khanal
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "Header.h"
+#include "Timerr.h"
+#include "Pins.h"
+#include "Controls.h"
+#include "uart.h"
+#include "max31865.h"
+#include "max7219.h"
+#include "led.h"
+
+#define BAUD 9600
+#define BUFF_LEN 700
+#define BAUD_PRESCALE (((F_CPU / (BAUD * 16UL))) - 1)
+
+
+#define AIR_SENSOR 1
+#define SKIN_SENSOR 0
+
+
+void init_devices();
+void check();
+void sendToDisplay(float air, float skin, float set);
+void sendToDisplayBelow(float temp);
+void displaySensFail();
+void displayPreHeat();
+void displaySetTemperature();
+void displayON();
+void displayOFF();
+void displayBABY();
+void titititi();
+void titi();
+
+//monitoring for
+bool startMonitor = false;
+int second = 0;
+
+
+//TIMER FOR DISPLAY UPDATE
+Timerr timerr;
+volatile bool displayUpdate = false;
+
+//FAN HEATER BUZZER
+Controls controls;
+bool start_buzzer = false;
+int buzzer_count = 0;
+bool buzzer_timer = false;
+
+//PT100 TEMPERATURE
+Adafruit_MAX31865 maxAir;
+Adafruit_MAX31865 maxSkin;
+
+float skin_temperature = 0.0;
+float air_temperature = 0.0;
+float set_temperature = 35.0;
+float set_temperature_copy = set_temperature;
+float pre_heat_max_temp = 30.0;
+
+volatile int display_count = 0;
+volatile int display_count_max = 2;
+int optimumTemp = 35;
+
+//DISPLAY
+max7219 max1;
+max7219 max2;
+
+//LED
+led Led;
+
+//buttons
+bool set_up_pressed = false;
+bool set_pressed = false;
+bool set_down_pressed = false;
+bool buzzer_stop_pressed = false;
+bool stop_buzzer = false;
+bool button_change = false;
+bool emergencyAlert = false;
+
+//preheat heater
+bool preHeat = false;
+
+//nonstandard/checking variables
+uint32_t countpreheat = 0;
+
+int main(void)
+{	
+	
+	
+	init_devices();
+	_delay_ms(1000);
+	
+	initUART0();
+	//titi();
+	//initialize local variables.
+	sendToDisplay(air_temperature,skin_temperature, set_temperature);
+	
+	float temperature_heater_off = 0.0;
+	float temperature_heater_on = 0.0;
+	//
+	//wait while preheat condition is meet.
+	bool clear_display = false;
+	bool set_temp_display = false;
+	
+	UART0TransmitString("On\r\n");
+	while(!preHeat){
+		
+		if(displayUpdate){
+			clear_display = !clear_display;
+			
+			displayUpdate = false;
+			maxSkin.begin(SKIN_SENSOR);
+			skin_temperature = maxSkin.temperature(100.0,430.0);
+			max1.MAX7219_init(REINITIALIZE);
+			max2.MAX7219_init(REINITIALIZE);
+			displayPreHeat();
+			if(clear_display) {
+				max2.MAX7219_clearDisplay();
+				//max1.MAX7219_clearDisplay();
+				} else {
+				sendToDisplayBelow(skin_temperature);
+			}
+			if(skin_temperature >= pre_heat_max_temp) {
+				controls.stopHeater();
+				preHeat = true;
+				titititi();
+			}
+			else if(skin_temperature < pre_heat_max_temp) {
+				controls.startHeater();
+			}
+		}
+		
+		else{
+			asm volatile ("nop");
+		}
+		
+	}
+	while(1)
+	{
+		check();
+		if(displayUpdate) {
+			//UART0TransmitString("On\r\n");
+			
+			//spi for temperature pt100
+			maxAir.begin(AIR_SENSOR); //these are init
+			maxSkin.begin(SKIN_SENSOR);
+			skin_temperature = maxSkin.temperature(100.0, 430.0);
+			air_temperature = maxAir.temperature(100.0, 430.0);
+			max1.MAX7219_init(REINITIALIZE); // these are init
+			max2.MAX7219_init(REINITIALIZE);
+			
+			//buzzer karaying
+			if(start_buzzer) {
+				buzzer_timer = !buzzer_timer;
+				if(buzzer_timer) {
+					//  UART0TransmitString("On\r\n");
+					controls.startBuzzer();
+					} else {
+					// UART0TransmitString("off\r\n");
+					controls.stopBuzzer();
+				}
+			}
+			// sending this
+			if(button_change){
+				set_temp_display = !set_temp_display;
+				if(set_temp_display) {
+					sendToDisplayBelow(set_temperature_copy);
+					} else {
+					max2.MAX7219_clearDisplay();
+				}
+				displaySetTemperature();
+			}
+			
+			else{
+				sendToDisplay(air_temperature, skin_temperature, set_temperature);
+			}
+			
+			temperature_heater_off = set_temperature - 0.1f;
+			temperature_heater_on = set_temperature - 0.2f;
+			
+			if(skin_temperature >= temperature_heater_off) {
+				controls.stopHeater();
+				}else if(skin_temperature <= temperature_heater_on) {
+				controls.startHeater();
+			}
+			
+			displayUpdate = false;
+		}
+		else{
+			asm volatile ("nop");
+		}
+	}
+}
+
+
+
+void init_devices() {
+	
+	//_delay_ms(1000);	
+	
+	sei();
+	
+	//heater and buzzer
+	controls.initControls();
+
+	//led
+	Led.led_init();
+	//Led.led_all(true);
+	//for time
+	timerr.setTimerNum(1);
+	timerr.setCompareInterrupt();
+	timerr.startCustomTimer(200);
+	
+	//start buzzer
+	
+	//for pt100
+	//maxAir.begin(1);
+	//maxSkin.begin(0);
+	
+	//skin_temperature = maxSkin.temperature(100.0, 430.0);
+	//air_temperature = maxAir.temperature(100.0, 430.0);
+	
+	//for display
+	//max1.MAX7219_init();
+	//Led.led_all(true);
+	max1.MAX7219_set(0,4,4);
+	
+	_delay_ms(10);
+	max2.MAX7219_set(1, 4, 4);
+	_delay_ms(10);
+	
+	max1.MAX7219_init(FIRSTINITIALIZE);
+	_delay_ms(10);
+	max2.MAX7219_init(FIRSTINITIALIZE);
+	_delay_ms(10);
+	max1.MAX7219_clearDisplay();
+	_delay_ms(30);
+	max2.MAX7219_clearDisplay();
+	_delay_ms(30);
+}
+
+ISR(TIMER1_COMPA_vect) {
+	TCNT1 = 0;
+	if(startMonitor) {
+		second++;
+	}
+	display_count++;
+	if(display_count >= display_count_max) {
+		displayUpdate = true;
+		display_count = 0;
+	}
+}
+
+void check() {
+	
+	if(skin_temperature >= (set_temperature + 0.2f) ) {
+		if(!startMonitor) {
+			//	UART0TransmitString("true\t");
+			start_buzzer = true;
+		}
+		
+		if(stop_buzzer && !startMonitor) {
+			//UART0TransmitString("stop \t");
+			startMonitor = true;
+			second = 0;
+			stop_buzzer = false;
+			start_buzzer = false;
+			//controls.stopBuzzer();
+		}
+		//UART0TransmitString("\r\n");
+		Led.led_do(TS_HIGH_LED, 1);
+		//Led.led_do(TA_HIGH_LED, 1);
+	}
+	else {
+		//	UART0TransmitString("else\r\n");
+		controls.stopBuzzer();
+		start_buzzer = false;
+		Led.led_do(TS_HIGH_LED, 0);
+		startMonitor = false;
+		second = 0;
+	}
+	
+	if(second >= 600) {	//timer prescaled to 200ms so 600 means 2 minutes.
+		//why this below line needed when if startMonitor = false, and skin temperature still set_temperature + 0.2
+		//controls.startBuzzer();
+		startMonitor = false;
+		second = 0;
+	}
+	
+	if(air_temperature > 37) {
+		//controls.startBuzzer();
+		//Led.led_do(TS_HIGH_LED, 1);
+		Led.led_do(TA_HIGH_LED, 1);
+		} else{
+		//controls.stopBuzzer();
+		Led.led_do(TS_HIGH_LED, 0);
+	}
+	
+	//buzzer stop button
+	if(bit_is_clear(BUZZER_STOP_BUTTON_PORT, BUZZER_STOP_BUTTON_PIN) && !buzzer_stop_pressed ) {
+		//UART0TransmitString("\r\n bhoot \r\n");
+		controls.stopBuzzer();
+		stop_buzzer = true;
+		buzzer_stop_pressed = true;
+	}
+	
+	else if(bit_is_set(BUZZER_STOP_BUTTON_PORT, BUZZER_STOP_BUTTON_PIN)) {
+		buzzer_stop_pressed = false;
+	}
+	
+	//set up button
+	
+	if(bit_is_clear(SET_UP_BUTTON_PORT, SET_UP_BUTTON_PIN) && !set_up_pressed) {
+		
+		if(button_change) {
+			set_temperature_copy += 0.1f;
+		}
+		set_up_pressed = true;
+	}
+	
+	else if(bit_is_set(SET_UP_BUTTON_PORT, SET_UP_BUTTON_PIN)) {
+		set_up_pressed = false;
+	}
+	
+	
+	//set down button
+	if(bit_is_clear(SET_DOWN_BUTTON_PORT, SET_DOWN_BUTTON_PIN) && !set_down_pressed) {
+		
+		if(button_change) {
+			set_temperature_copy -= 0.1f;
+		}
+		
+		set_down_pressed= true;
+	}
+	
+	else if(bit_is_set(SET_DOWN_BUTTON_PORT, SET_DOWN_BUTTON_PIN)) {
+		set_down_pressed = false;
+	}
+	
+	//set button
+	if(bit_is_clear(SET_BUTTON_PORT, SET_BUTTON_PIN) && !set_pressed) {
+		button_change = !button_change;
+		if(!button_change) {
+			set_temperature = set_temperature_copy;
+			titi();
+		}
+		
+		set_pressed = true;
+	}
+	
+	else if(bit_is_set(SET_BUTTON_PORT, SET_BUTTON_PIN)) {
+		set_pressed = false;
+	}
+	
+}
+
+void sendToDisplay(float air, float skin, float set) {
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0xFF);
+	max1.MAX7219_writeData(8, air / 10);
+	max1.MAX7219_writeData(6, ((int)air % 10) | 0b10000000);
+	
+	max1.MAX7219_writeData(2, (int)(air * 10) % 10);
+	
+	max1.MAX7219_writeData(7, skin / 10);
+	max1.MAX7219_writeData(5, ((int)skin % 10) | 0b10000000);
+	
+	max1.MAX7219_writeData(1, (int)(skin * 10) % 10);
+	
+	max2.MAX7219_writeData(MAX7219_MODE_DECODE,0xFF);
+	//max2.MAX7219_writeData(1, set / 10);
+	
+	//max2.MAX7219_writeData(5, (int)set % 10);
+	//max2.MAX7219_writeData(4, (int)(set*10) % 10);
+	max2.MAX7219_writeData(6, (set / 10));
+	
+	max2.MAX7219_writeData(8, ((int)set % 10)| 0b10000000);
+	max2.MAX7219_writeData(2, (int)(set*10) % 10);
+}
+
+
+void sendToDisplayBelow(float temp){
+	max2.MAX7219_writeData(MAX7219_MODE_DECODE,0xFF);
+	max2.MAX7219_writeData(6, (temp / 10));
+	max2.MAX7219_writeData(8, ((int)temp % 10)| 0b10000000);
+	max2.MAX7219_writeData(2, (int)(temp*10) % 10);
+}
+
+void displaySensFail()
+{
+	
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,S);
+	max1.MAX7219_writeData(7,E);
+	max1.MAX7219_writeData(5,N);
+	max1.MAX7219_writeData(1,S);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,F);
+	max1.MAX7219_writeData(8,A);
+	max1.MAX7219_writeData(6,I);
+	max1.MAX7219_writeData(2,L);
+}
+
+void displaySetTemperature(){
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,das);
+	max1.MAX7219_writeData(7,S);
+	max1.MAX7219_writeData(5,E);
+	max1.MAX7219_writeData(1,t);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,t);
+	max1.MAX7219_writeData(8,P);
+	max1.MAX7219_writeData(6,das);
+	max1.MAX7219_writeData(2,das);
+}
+
+void displayPreHeat(){
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,das);
+	max1.MAX7219_writeData(7,P);
+	max1.MAX7219_writeData(5,R);
+	max1.MAX7219_writeData(1,E);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,H);
+	max1.MAX7219_writeData(8,E);
+	max1.MAX7219_writeData(6,A);
+	max1.MAX7219_writeData(2,t);
+}
+
+void displayON(){
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,das);
+	max1.MAX7219_writeData(7,O);
+	max1.MAX7219_writeData(5,N);
+	max1.MAX7219_writeData(1,das);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,das);
+	max1.MAX7219_writeData(8,O);
+	max1.MAX7219_writeData(6,F);
+	max1.MAX7219_writeData(2,F);
+	
+}
+
+void displayOFF(){
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,das);
+	max1.MAX7219_writeData(7,O);
+	max1.MAX7219_writeData(5,N);
+	max1.MAX7219_writeData(1,das);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,das);
+	max1.MAX7219_writeData(8,O);
+	max1.MAX7219_writeData(6,F);
+	max1.MAX7219_writeData(2,F);
+}
+
+void displayBABY(){
+	max1.MAX7219_writeData(MAX7219_MODE_DECODE,0x00);
+	//first 4-7segment display from left to right
+	max1.MAX7219_writeData(3,B);
+	max1.MAX7219_writeData(7,A);
+	max1.MAX7219_writeData(5,B);
+	max1.MAX7219_writeData(1,Y);
+	
+	//second 4-7 segment display from left to right
+	max1.MAX7219_writeData(4,B);
+	max1.MAX7219_writeData(8,A);
+	max1.MAX7219_writeData(6,B);
+	max1.MAX7219_writeData(2,Y);
+}
+// Order of right and left 4-7 segment matrix registers.
+// 3 7 5 1      4 8 6 2
+
+
+void titititi(){
+	controls.startBuzzer();
+	_delay_ms(100);
+	controls.stopBuzzer();
+	_delay_ms(100);
+	controls.startBuzzer();
+	_delay_ms(100);
+	controls.stopBuzzer();
+	_delay_ms(300);
+	controls.startBuzzer();
+	_delay_ms(100);
+	controls.stopBuzzer();
+	_delay_ms(100);
+	controls.startBuzzer();
+	_delay_ms(100);
+}
+
+void titi(){
+	controls.startBuzzer();
+	_delay_ms(100);
+	controls.stopBuzzer();
+	_delay_ms(100);
+	controls.startBuzzer();
+	_delay_ms(100);
+	controls.stopBuzzer();
+	_delay_ms(100);
+}
---
+++ BabyWarmerMega/BabyWarmerUno/pins.h
@@ -0,0 +1,84 @@
+/*
+ * pins.h
+ *
+ * Created: 5/31/2019 2:16:45 AM
+ *  Author: swain
+ */ 
+
+
+#ifndef PINS_H_
+#define PINS_H_
+
+#define DDR(x) (*(&x - 1))      /* address of data direction register of port x */
+
+//For Heater Control /
+#define HEATER_PORT PORTD
+#define HEATER_PIN PIND3
+
+//Buzzer
+#define BUZZER_PORT PORTH
+#define BUZZER_PIN PINH4
+
+//LED
+
+#define SENSOR_FAIL_PORT PORTH //
+#define SENSOR_FAIL_PIN PINH3     
+
+#define HEATER_FAIL_PORT PORTE
+#define HEATER_FAIL_PIN PINE3
+
+#define TS_HIGH_PORT PORTL
+#define TS_HIGH_PIN PINL1
+
+#define TA_HIGH_PORT PORTL
+#define TA_HIGH_PIN PINL5
+
+#define POWER_PORT PORTG //
+#define POWER_PIN PING5
+
+#define SET_TEMP_PORT PORTL  //
+#define SET_TEMP_PIN PINL3
+
+
+//BUTTONS
+#define SET_UP_BUTTON_PORT  PINB
+#define SET_UP_BUTTON_PIN PINB4
+
+#define SET_BUTTON_PORT PINH
+#define SET_BUTTON_PIN PINH6
+
+#define SET_DOWN_BUTTON_PORT PINH
+#define SET_DOWN_BUTTON_PIN PINH5
+
+#define BUZZER_STOP_BUTTON_PORT PINE
+#define BUZZER_STOP_BUTTON_PIN PINE5
+
+
+//SPI SLAVE SELECTS
+#define DISPLAY_I_PORT PORTK
+#define DISPLAY_I_PIN PINK7
+
+#define DISPLAY_II_PORT PORTK
+#define DISPLAY_II_PIN PINK6
+
+#define TEMP_I_PORT PORTL
+#define TEMP_I_PIN PINL0
+
+#define TEMP_II_PORT PORTL
+#define TEMP_II_PIN PINL2
+
+//SPI PINS
+#define MOSI_PORT	PORTB
+#define SCK_PORT	PORTB
+#define MISO_PORT	PORTB
+#define SS_PORT		PORTB
+
+#define SCK_PIN    PINB1
+#define MOSI_PIN   PINB2
+#define MISO_PIN   PINB3
+#define SS_PIN    PINB0
+
+
+
+
+#endif /* PINS_H_ */
